f78c8f0ff857dcf7d6c34978194c9c55127392f1
haikelassaker <haikel.assaker@gmail.com>
2018-11-27 13:32:08 +0100

    Beispiel ein Rezept

--- Alfa-Back-Console/lagerVerwaltung.cpp
+++ Alfa-Back-Console/lagerVerwaltung.cpp
@@ -2,13 +2,14 @@
 #include "lagerVerwaltung.h"
 #include <iostream>
 using namespace std;
-
+/**
 lagerVerwaltung::lagerVerwaltung(map<string, zutat> lagerBestandZutaten, map<string, zutat> lagerBestandVerzierungen)
 {
 	lagerBestandZt=lagerBestandZutaten;
 	lagerBestandVezgen=lagerBestandVerzierungen;
 
 }
+*/
 void lagerVerwaltung::setEingangwareVerzierungen(zutat eingangswareVerzierungen)
 {
 	lagerBestandVezgen.insert(pair<string, zutat>(eingangswareVerzierungen.getName(), eingangswareVerzierungen));
@@ -30,25 +31,32 @@ bool lagerVerwaltung::pruefeLageBestand(map<string,zutat> zutatMenge, map<string
 	// Schleife zu ueberpruefen von Verfuegbarkeit des Zutaten Bestandes
 	for (map<string, zutat>::iterator it = lagerBestandZt.begin(); it != lagerBestandZt.end(); it++)
 	{
-		if ((it->second).getMenge() >= (it_zutatMenge->second).getMenge())
+		if ((((it->second).getName()).compare(it_zutatMenge->second.getName())))
 		{
-			lagerBestandZt[it->first].setMenge( ( ( (it->second).getMenge()) - (it_zutatMenge->second).getMenge() )   );
+			if (((it->second).getMenge() >= (it_zutatMenge->second).getMenge()))
+			{
+				lagerBestandZt[it->first].setMenge((((it->second).getMenge()) - (it_zutatMenge->second).getMenge()));
+			}
+			else
+			{
+				lageZustandZutat = false;
+			}
 		}
-		else
-		{
-			lageZustandZutat = false;
-		}
+		
 	}
 	// Schleife zu ueberpruefen von Verfuegbarkeit der Verzierungen Bestandes
 	for (map<string, zutat>::iterator it = lagerBestandVezgen.begin(); it != lagerBestandVezgen.end(); it++)
 	{
-		if ((it->second).getMenge() >= (it_verzierungMenge->second).getMenge())
+		if ((((it->second).getName()).compare(it_verzierungMenge->second.getName())))
 		{
-			lagerBestandVezgen[it->first].setMenge( ((it->second).getMenge()) - ((it_verzierungMenge->second).getMenge()));
-		}
-		else
-		{
-			lageZustandVerzierungen = false;
+			if ((it->second).getMenge() >= (it_verzierungMenge->second).getMenge())
+			{
+				lagerBestandVezgen[it->first].setMenge(((it->second).getMenge()) - ((it_verzierungMenge->second).getMenge()));
+			}
+			else
+			{
+				lageZustandVerzierungen = false;
+			}
 		}
 	}
 
@@ -59,12 +67,16 @@ void lagerVerwaltung::lagerBestandAnzeigen()
 {
 	for (map<string, zutat>::iterator it = lagerBestandZt.begin(); it != lagerBestandZt.end(); it++)
 	{
-		cout << "Menge an Verzierungsorte " << (it->second).getName() << " ist " << (it->second).getMenge() << endl;
+		cout << "Menge an Zutaten: " << (it->second).getName() << " ist " << (it->second).getMenge() << endl;
 	}
 	for (map<string, zutat>::iterator it = lagerBestandVezgen.begin(); it != lagerBestandVezgen.end(); it++)
 	{
-		cout << "Menge an Zutaten Sorte " << (it->second).getName() << " ist " << (it->second).getMenge() << endl;
+		cout << "Menge an Verzierungen: " << (it->second).getName() << " ist " << (it->second).getMenge() << endl;
 	}
+}
+
+lagerVerwaltung::lagerVerwaltung()
+{
 }
 
 
